6910c6b5b2b00f80aca36c944e8be1fa5b2ffd9a
Nate Catelli <ncatelli@packetfire.org>
2021-08-20 00:20:40 +0000

    remove comments

--- src/lib.rs
+++ src/lib.rs
@@ -771,38 +771,7 @@ where
     V: PartialEq + PartialOrd + Default + 'a,
 {
     type Item = &'a V;
-    /*
-    # Set current to root of binary tree
-    current = root
-    stack = [] # initialize stack
-    done = 0
 
-    while True:
-
-        # Reach the left most Node of the current Node
-        if current is not None:
-
-            # Place pointer to a tree node on the stack
-            # before traversing the node's left subtree
-            stack.append(current)
-
-            current = current.left
-
-
-        # BackTrack from the empty subtree and visit the Node
-        # at the top of the stack; however, if the stack is
-        # empty you are done
-        elif(stack):
-            current = stack.pop()
-            print(current.data, end=" ") # Python 3 printing
-
-            # We have visited the node and its left
-            # subtree. Now, it's right subtree's turn
-            current = current.right
-
-        else:
-            break
-    */
     fn next(&mut self) -> Option<Self::Item> {
         while let Some(id) = self.left_most_node {
             self.stack.push(id);
